5f753b318957fb38a5f68c15cbf56d1a36e61355
Ryan Liew <fatal-fates@hotmail.com>
2017-06-18 00:34:18 +0800

    Completed address search

--- resources/assets/js/components/Order.vue
+++ resources/assets/js/components/Order.vue
@@ -4,16 +4,55 @@
 
 <script>
 	import moment from 'moment';
+
 	export default {
 		data() {
 			return {
 				delivery_time: moment().format("MM-DD-YYYY hh:mm"),
 				total: 0,
 				driver_id: 0,
-				delivery_location: "Somewhere in the forest",
+				delivery_location: "Current location",
+				location_type: "Current location",
 				action: "Search driver",
-				is_loading: false
-			};
+				is_loading: false,
+				map_center: {lat: 0, lng: 0},
+				marker_position: {lat: 0, lng: 0},
+				zoom: 17,
+				place: null,
+				infoWinOpen: false,
+				infoOptions: {
+		            pixelOffset: {
+		              	width: 0,
+		              	height: -35
+		            }
+		        },	
+		        infoWindowPos: {lat: 0, lng: 0},
+		        route: "",
+		        postcode: "",
+		        state: "",
+		        city: "",
+		        unit: ""
+			}
+		},
+
+		computed: {
+			infoContent() {
+				return "<b>" + this.location_type +"</b>" + "<br>" + this.delivery_location;			
+			}
+		},
+
+		mounted() {
+			this.is_loading = true;
+			if ('geolocation' in navigator) {
+		        var gl = navigator.geolocation;
+		        gl.getCurrentPosition(function(position) {
+		          	this.map_center.lat = position.coords.latitude;
+		          	this.map_center.lng = position.coords.longitude;
+		          	this.marker_position.lat = position.coords.latitude;
+		          	this.marker_position.lng = position.coords.longitude;
+		        }.bind(this));
+		        this.is_loading = false;
+		    }	
 		},
 
 		methods: {
@@ -26,8 +65,70 @@
 					// On success, create an order
 					 
 				}
-			}
-		}
+			},
+			updateCenter(newCenter) {
+				this.map_center = {
+					lat: newCenter.lat(),
+					lng: newCenter.lng(),
+				}
+			},
+			updateMarkerCenter() {
 
+				marker_position = this.$refs.markdelivery.position;
+				this.location_type = "Custom location";
+				this.delivery_location = marker_position.lat + " " + marker_position.lng;
+				this.address = "";
+
+			},
+			setPlace(place) {
+				this.$refs.markdelivery.position.lat = place.geometry.location.lat();
+				this.$refs.markdelivery.position.lng = place.geometry.location.lng();
+				this.updateCenter(place.geometry.location);
+				this.location_type = "Found place";
+				this.delivery_location = place.adr_address;
+				this.updateAddressForm(place);
+
+			},
+			toggleInfoWindow() {
+				this.infoWindowPos = this.marker_position;
+				this.infoContent = this.delivery_location;
+
+				this.infoWinOpen = !this.infoWinOpen;	
+			},
+
+			updateAddressForm(place) {
+				for(var i = 0; i < place.address_components.length; i++)
+				{
+					var component = place.address_components[i]
+					for(var j = 0; j < component.types.length; j++)
+					{
+
+						var type = place.address_components[i].types[j];
+						if(type == 'route')
+						{
+							this.route = component.long_name;
+						}
+						if(type == 'postal_code')
+						{
+							this.postcode = component.long_name;
+						}
+						if(type == 'locality')
+						{
+							this.city = component.long_name;
+						}
+						if(type == 'street_number')
+						{
+							this.unit = component.long_name;
+						}
+						if(type == 'administrative_area_level_1')
+						{
+							this.state = component.long_name;
+						}
+					}
+				}
+
+			}
+
+		}
 	}
 </script>
--- resources/views/users/order.blade.php
+++ resources/views/users/order.blade.php
@@ -14,16 +14,38 @@
                         </div>
                         <div class="form-group">
                             <label for="driver_id">Delivery location:</label>
-                            <input type="text" class="form-control" id="delivery_location" name="delivery_location" v-model="delivery_location">
+                            <gmap-autocomplete class="form-control"
+                                @place_changed="setPlace">
+                            </gmap-autocomplete>
+                        </div>
+                        <gmap-map :center="map_center" :zoom="zoom" style="height:300px" :scrollWheel="true" @center_changed="updateCenter">
+                            <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" :content="infoContent" @closeclick="infoWinOpen=false"></gmap-info-window>
+                            <gmap-marker :position="marker_position" ref="markdelivery" :draggable="true" :clickable="true" @click="toggleInfoWindow()" @dragend="updateMarkerCenter"></gmap-marker>
+                            
+                        </gmap-map>
+                        
+                        <h3>Delivery Address</h3>
+                        <div class="form-group">
+                            <label for="total">Unit/House number:</label>
+                            <input type="text" class="form-control" id="total" name="total" v-model="unit" required>
                         </div>
                         <div class="form-group">
-                            <label for="total">Total:</label>
-                            <input type="text" class="form-control" id="total" name="total" v-model="total">
+                            <label for="total">Route:</label>
+                            <input type="text" class="form-control" id="total" name="total" v-model="route" required>
                         </div>
                         <div class="form-group">
-                            <label for="driver_id">Driver ID:</label>
-                            <input type="text" class="form-control" id="driver-id" name="driver_id" v-model="driver_id">
+                            <label for="total">City:</label>
+                            <input type="text" class="form-control" id="total" name="total" v-model="city" required>
                         </div>
+                        <div class="form-group">
+                            <label for="total">Postcode:</label>
+                            <input type="text" class="form-control" id="total" name="total" v-model="postcode" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="total">State:</label>
+                            <input type="text" class="form-control" id="total" name="total" v-model="state" required>
+                        </div>
+                        
                         <button type="submit" class="btn btn-primary" v-text="action" @click="execute"></button>
 
                     </div>
