6831ae01e1d222b37d7c11b2147bf814e6161966
Ryan Liew <fatal-fates@hotmail.com>
2017-06-25 18:00:07 +0800

    Added logs

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,6 +10,8 @@ class APIController extends Controller
     //
     public function getDriverResult(Request $request)
     {
+    	Log::info("Request received!");
+    	Log::info("For order: " . $request->order_id );
     	event(new DriverResultReturned($request->order_id, 
     								   $request->status,
     								   $request->driver_id,
